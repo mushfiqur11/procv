2f518032b492bda075dda659f30659e888621a00
Md Mushfiqur Rahman <mushfiqur11@iut-dhaka.edu>
2023-03-21 15:22:21 -0400

    Fixed host address in main

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import logging
 import uvicorn
 import argparse
 from app import *
-def start_server(host = "https://localhost/",port = 8000):
+def start_server(host = "127.0.0.1",port = 8000):
     '''
     The functions starts the FastAPI server in port 8000
     '''
     uvicorn.run("app.app:app", 
-                host= host,
+                host=host,
                 port = port,
                 reload=True)
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default="https://localhost/", help="Host address")
+    parser.add_argument('--host', type=str, default="localhost", help="Host address")
     parser.add_argument('--port', type=int, default=8000, help="Host port")
     args = parser.parse_args()
 
